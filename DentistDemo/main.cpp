a2f06854b8d282796f4a900cd79aef2ecfca7752
Dark <darkgrouptw@gmail.com>
2018-08-03 11:53:45 +0800

    上傳空專案

---
+++ DentistDemo/main.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	DentistDemo::MyForm form;
+	Application::Run(%form);
+
+	return 0;
+}
